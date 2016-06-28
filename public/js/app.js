3b420f4f656eba368923648025d217f659c9b758
UberBooster <info@uberbooster.com>
2016-06-27 20:54:58 -0400

    the grid is working

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-  console.log("Off to the races");
+var container = $('.container');
+var numOfRows = 46;
+var numOfCols = 46;
+
+initGrid();
+function initGrid(){
+  for(var i = 0; i < numOfRows; i += 1){
+    var row = $('<div></div>'); //creates a new element in your HTML
+    for(var j = 0; j < numOfCols; j += 1){
+      var cell = $('<div></div>');
+      cell.addClass('cell border');
+      row.append(cell);
+    }
+    container.append(row);
+  }
+};
+
+
 });
