17ffaedb8208e8d30e1c1e60b37161800e550243
UberBooster <info@uberbooster.com>
2016-06-28 18:36:15 -0400

    completed grid payout

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 var container = $('.container');
-var numOfRows = 46;
-var numOfCols = 46;
+var numOfRows = 10;
+var numOfCols = 10;
 
 initGrid();
 function initGrid(){
   for(var i = 0; i < numOfRows; i += 1){
     var row = $('<div></div>'); //creates a new element in your HTML
+    row.addClass('row');
     for(var j = 0; j < numOfCols; j += 1){
       var cell = $('<div></div>');
       cell.addClass('cell border');
