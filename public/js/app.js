971251bc5875b438eab717adf220709fc8211300
UberBooster <info@uberbooster.com>
2016-06-28 19:01:01 -0400

    set up click handlers

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,10 @@ var numOfRows = 10;
 var numOfCols = 10;
 
 initGrid();
+addClickHandlers();
+
+
+
 function initGrid(){
   for(var i = 0; i < numOfRows; i += 1){
     var row = $('<div></div>'); //creates a new element in your HTML
@@ -16,6 +20,19 @@ function initGrid(){
     container.append(row);
   }
 };
+
+function addClickHandlers(){
+  var cells = $('.cell');
+  for(var counter = 0; counter < cells.length; counter += 1){
+    var cell = cells[counter];
+    $(cell).on('click', changeColor);
+  }
+  console.log(cells);
+};
+
+function changeColor(){
+  console.log("I am changing colors...");
+}
 
 
 });
